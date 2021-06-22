6392aed1c3d6f5798d4de99961a23f489f03567a
huydevct <huydevct@gmail.com>
2021-06-22 23:26:01 +0700

    username and clock

---
+++ js/greetings.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const onLoginSubmit = (event) => {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  paintingGreetings(username);
+};
+
+const paintingGreetings = (username) => {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintingGreetings(savedUsername);
+}
