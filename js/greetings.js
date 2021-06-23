6fb1c5122d41e8cc3d2efc7cd9100490149e139e
huydevct <huydevct@gmail.com>
2021-06-23 11:24:32 +0700

    todo

--- js/greetings.js
+++ js/greetings.js
@@ -5,18 +5,18 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-const onLoginSubmit = (event) => {
+function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  paintingGreetings(username);
-};
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
 
-const paintingGreetings = (username) => {
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-};
+}
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
@@ -24,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintingGreetings(savedUsername);
+  paintGreetings(savedUsername);
 }
